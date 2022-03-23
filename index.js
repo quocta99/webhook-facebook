e83423768c3ea836764af3751ffac3086e2f6e02
quoc.ta <dev@visualweber.com>
2022-03-23 23:48:28 +0700

    -

--- index.js
+++ index.js
@@ -32,22 +32,24 @@ app.get('/webhook', function (req, res) {
 // Đoạn code xử lý khi có người nhắn tin cho bot
 app.post('/webhook', function (req, res) {
     var entries = req.body.entry;
-    for (var entry of entries) {
-        var messaging = entry.messaging;
-        for (var message of messaging) {
-            var senderId = message.sender.id;
-            if (message.message) {
-                // Nếu người dùng gửi tin nhắn đến
-                if (message.message.text) {
-                    var text = message.message.text;
-                    if (text == 'hi' || text == "hello") {
-                        sendMessage(senderId, "Trung Quân's Bot: " + 'Xin Chào');
-                    }
-                    else { sendMessage(senderId, "Trung Quân's Bot: " + "Xin lỗi, câu hỏi của bạn chưa có trong hệ thống, chúng tôi sẽ cập nhật sớm nhất."); }
-                }
-            }
-        }
-    }
+
+    console.log({entries});
+    // for (var entry of entries) {
+    //     var messaging = entry.messaging;
+    //     for (var message of messaging) {
+    //         var senderId = message.sender.id;
+    //         if (message.message) {
+    //             // Nếu người dùng gửi tin nhắn đến
+    //             if (message.message.text) {
+    //                 var text = message.message.text;
+    //                 if (text == 'hi' || text == "hello") {
+    //                     sendMessage(senderId, "Trung Quân's Bot: " + 'Xin Chào');
+    //                 }
+    //                 else { sendMessage(senderId, "Trung Quân's Bot: " + "Xin lỗi, câu hỏi của bạn chưa có trong hệ thống, chúng tôi sẽ cập nhật sớm nhất."); }
+    //             }
+    //         }
+    //     }
+    // }
 
     res.status(200).send("OK");
 });
